9a73818c8d421e372f14887d28d9881290d5979f
Travis DePrato <travisdeprato@gmail.com>
2021-04-27 21:40:46 -0700

    Add publish to CI workflow

--- ipydraw/_version.py
+++ ipydraw/_version.py
@@ -4,5 +4,6 @@
 # Copyright (c) Travis DePrato.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 1, 0, 'dev')
-__version__ = ".".join(map(str, version_info))
+version_info = (0, 1, 1)
+version_suffix = "+alpha.0"
+__version__ = ".".join(map(str, version_info)) + version_suffix
