6fdc5855ba4010db28569df40f007c42a6ea01fa
Travis DePrato <travisdeprato@gmail.com>
2021-04-27 22:15:38 -0700

    Tweak CI publish config

--- ipydraw/_version.py
+++ ipydraw/_version.py
@@ -5,5 +5,5 @@
 # Distributed under the terms of the Modified BSD License.
 
 version_info = (0, 1, 1)
-version_suffix = "+alpha.4"
+version_suffix = "+alpha.5"
 __version__ = ".".join(map(str, version_info)) + version_suffix
