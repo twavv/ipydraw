0469bdaef7e3a6c0811e139923138fe53d34ca6e
Travis DePrato <travisdeprato@gmail.com>
2021-06-22 10:37:03 -0700

    v0.2.1

--- ipydraw/_version.py
+++ ipydraw/_version.py
@@ -4,7 +4,7 @@
 # Copyright (c) Travis DePrato.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 2, 0)
+version_info = (0, 2, 1)
 
 # For pre-release versions, use a7 format (for alpha.7)
 # For release versions, this should be empty.
